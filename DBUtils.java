4681bdba2a6ec1a83d7402c630947e67d99fe7b0
HEMANTHKUMAR MN <95921220+HemanthdkumarMN-devOps@users.noreply.github.com>
2021-12-22 19:46:04 +0530

    Update DBUtils.java

--- DBUtils.java
+++ DBUtils.java
@@ -4,3 +4,4 @@ one day definatly i will become an IAS Officer
 with my hardwork and determination...
 -> Dream LBSNAA "Lal Bahadur shastri National Academy Of Administration"
 Karnataka crader @ 2026
+iam working assistance district collector in Tumkur(district)
